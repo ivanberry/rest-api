12136dd74455ab6ea15e215a6d038deed658bd7a
ivanberry <tabvim.dev@gmail.com>
2018-12-08 10:11:27 +0800

    Add bcryt to hash password

--- models/account.go
+++ models/account.go
@@ -4,7 +4,9 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/ivanberry/rest-api/utils"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 	"os"
+	"strings"
 )
 
 type Account struct {
@@ -19,7 +21,46 @@ type Token struct {
 	jwt.StandardClaims
 }
 
+func (account *Account) Validate() (map[string]interface{}, bool) {
+
+	// validate emial
+	if !strings.Contains(account.Email, "@") {
+		return utils.Message(false, "请输入邮箱地址."), false
+	}
+
+	if len(account.Password) < 6 {
+		return utils.Message(false, "密码太弱."), false
+	}
+
+	temp := &Account{}
+
+	// check email duplicate
+	// 通过查询将获取的数据放置在了temp中?
+	err := GetDB().Table("account").Where("email = ?", account.Email).First(temp).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return utils.Message(false, "链接错误，请重试!"), false
+	}
+
+	if temp.Email != "" {
+		return utils.Message(false, "此邮箱已通过注册."), false
+	}
+
+	return utils.Message(true, "验证通过"), true
+
+}
+	
+
 func (account *Account) Create() (map[string]interface{}) {
+
+	if resp, ok := account.Validate(); !ok {
+		return resp
+	}
+
+	//Don't store the raw password, but the hash on
+
+	hashPassword, _ := bcrypt.GenerateFromPassword([]byte(account.Password), bcrypt.DefaultCost)
+	account.Password = string(hashPassword)
+
 	GetDB().Create(account)
 
 	if account.ID <= 0 {
@@ -32,10 +73,11 @@ func (account *Account) Create() (map[string]interface{}) {
 	tokenString, _ := token.SignedString([]byte(os.Getenv("token_password")))
 	account.Token = tokenString
 
+	// auto ignore nil value
+	account.Password = ""
 
 	response := utils.Message(true, "用户创建成功")
 	response["account"] = account
 	return response
-
 }
 
