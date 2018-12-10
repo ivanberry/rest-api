33f2f817f81e821d0779416412aca544c6cc270e
ivanberry <tabvim.dev@gmail.com>
2018-12-11 05:46:26 +0800

    Add post filter

--- models/post.go
+++ models/post.go
@@ -3,6 +3,7 @@ package models
 import (
 	"github.com/ivanberry/rest-api/utils"
 	"github.com/jinzhu/gorm"
+	"strings"
 )
 
 // 定义post结构提，用于数据存储
@@ -20,6 +21,10 @@ func (post *Post) Validate() (map[string]interface{}, bool) {
 
 	if len(post.Content) == 0 {
 		return utils.Message(false, "文章内容不能为空！"), false
+	}
+
+	if strings.Contains(post.Title, "色情") || strings.Contains(post.Content, "色情") {
+		return utils.Message(false, "包含敏感词"), false
 	}
 
 	return utils.Message(true, "内容合法！"), true
